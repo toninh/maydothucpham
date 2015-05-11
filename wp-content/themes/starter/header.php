56cc844e77317551a68a107855c53a499206048a
Ninh To Van <mu.toninh@gmail.com>
2015-05-11 11:40:25 +0700

    Add onlinechat

--- wp-content/themes/starter/header.php
+++ wp-content/themes/starter/header.php
@@ -33,7 +33,7 @@
 </head>
 	
 <body <?php body_class('woocommerce'); ?>>
-<script type='text/javascript'>window._sbzq||function(e){e._sbzq=[];var t=e._sbzq;t.push(["_setAccount",20136]);var n=e.location.protocol=="https:"?"https:":"http:";var r=document.createElement("script");r.type="text/javascript";r.async=true;r.src=n+"//static.subiz.com/public/js/loader.js";var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)}(window);</script>
+<script type='text/javascript'>window._sbzq||function(e){e._sbzq=[];var t=e._sbzq;t.push(["_setAccount",20195]);var n=e.location.protocol=="https:"?"https:":"http:";var r=document.createElement("script");r.type="text/javascript";r.async=true;r.src=n+"//static.subiz.com/public/js/loader.js";var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)}(window);</script>
 <!--<div class="home-slider-inner banner-loading"></div>-->
 <div id="page" class="hfeed site">
 <!-- Header -->
