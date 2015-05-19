df0e2c924614445f463fea6c8a10d82e98f8f9e9
Ninh To Van <mu.toninh@gmail.com>
2015-05-19 10:18:04 +0700

    Add Facebook Retarget Audience on website

--- wp-content/themes/starter/header.php
+++ wp-content/themes/starter/header.php
@@ -31,6 +31,22 @@
   ga('send', 'pageview');
 
 </script>
+<script>(function() {
+  var _fbq = window._fbq || (window._fbq = []);
+  if (!_fbq.loaded) {
+    var fbds = document.createElement('script');
+    fbds.async = true;
+    fbds.src = '//connect.facebook.net/en_US/fbds.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(fbds, s);
+    _fbq.loaded = true;
+  }
+  _fbq.push(['addPixelId', '441354832709948']);
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', 'PixelInitialized', {}]);
+</script>
+<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=441354832709948&amp;ev=PixelInitialized" /></noscript>
 </head>
 	
 <body <?php body_class('woocommerce'); ?>>
